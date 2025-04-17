c0845e4d5ec81369bf6e84f15ca26bbae27dd4ee
alejandroaguila27 <115678513+alejandroaguila27@users.noreply.github.com>
2025-04-16 23:53:19 -0600

    Update Algori

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'privacidad', component: PrivacyComponent},
 ];
